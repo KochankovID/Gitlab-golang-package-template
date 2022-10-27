cf2157fcf0ae68a80e27fdeb70796d7db3614e85
Илья Кочанков <ilyakochankov@yandex.ru>
2022-10-27 13:20:19 +0300

    ✨  add docs support

---
+++ {{cookiecutter.package_name}}/docs.go
@@ -0,0 +1,9 @@
+// Package {{cookiecutter.package_name}}
+//
+// {{cookiecutter.package_description}}
+//
+// # Licence
+//
+// Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
+
+package {{cookiecutter.package_name}}
